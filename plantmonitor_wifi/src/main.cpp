7424a0439e9f888a5e9ad8f9fc86b1a3c83cfc6c
Berend Swennenhuis <berendswennenhuis@gmail.com>
2019-02-11 22:44:57 +0100

    Added webserver to plantenbak

--- plantmonitor_wifi/src/main.cpp
+++ plantmonitor_wifi/src/main.cpp
@@ -5,12 +5,17 @@
 #include <ESP8266HTTPClient.h>
  
 // WiFi parameters to be configured
+const char* ssid = "swennenhuis"; // Hoofdlettergevoelig
+const char* password = "jambosana"; // Hoofdlettergevoelig
 
 const char* http_site = "http://195.240.135.237:4000/api/echo";
-const int http_port = 80;
+
+String header;
+
+WiFiServer server(80); 
 
 
-void setup(void) { 
+void setup() { 
   Serial.begin(9600);
   Serial.print("Bezig met verbinden");
   WiFi.begin(ssid, password); // Connect to WiFi
@@ -39,22 +44,90 @@ void setup(void) {
   Serial.print(rssi);
   Serial.println(" dBm");
   Serial.println("");
+
+  server.begin();
 }
+
+void postSensorData() {
+  HTTPClient http; //Declare an object of class HTTPClient
+  http.begin(http_site); //Specify request destination
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int httpCode = http.POST("Maar dit moet toch werken?"); //Send the request
+  if (httpCode > 0) { //Check the returning code
+    String payload = http.getString(); //Get the request response payload
+    Serial.println(payload); //Print the response payload
+  }
+  http.end(); //Close connection
+}
+
+void startServer() {
+  WiFiClient client = server.available();   // Listen for incoming clients
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP8266 Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 5  
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
+
+
 
 void loop() {
  if(WiFi.status()== WL_CONNECTED){ //Check WiFi connection status
-  
-    HTTPClient http; //Declare an object of class HTTPClient
-    http.begin(http_site); //Specify request destination
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    int httpCode = http.POST("Gaat dit?"); //Send the request
-    if (httpCode > 0) { //Check the returning code
-      String payload = http.getString(); //Get the request response payload
-      Serial.println(payload); //Print the response payload
-    }
-    http.end(); //Close connection
+    // postSensorData();
+    startServer();
   } else {
     Serial.println("Error in WiFi connection");   
   }
-  delay(30000); //Send a request every 30 seconds
+  // delay(30000); //Send a request every 30 seconds
 }
+
